1ad4bbbdb34c3318bb5b589dc32a8f29ad01b5a0
Eulena <46357661+Eulena@users.noreply.github.com>
2019-05-10 22:10:07 -0400

    Delete Item_7.sql

--- SQL_Data Analysis_ Queries_Table Schemata/Item_7.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-employees_data.emp_no employee_number,
-employees_data.last_name last_name,
-employees_data.first_name first_name,
-departments.dept_name department_name
-FROM
-employees_data NATURAL JOIN departments
-WHERE dept_no in ('d007', 'd005') ORDER BY department_name ASC;
-
